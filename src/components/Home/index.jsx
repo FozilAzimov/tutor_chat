a72d10d2f21ecc2b2f997a6f8c97188554e8f94a
FozilAzimov <foziljon.azimov@yandex.ru>
2023-07-27 01:35:59 +0500

    upate

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,7 +1,61 @@
 import React from 'react';
 
+import icon1 from '../../assets/icons/chemistry.svg';
+import icon2 from '../../assets/icons/maths.svg';
+import icon3 from '../../assets/icons/physics.svg';
+import icon4 from '../../assets/icons/astronomy.svg';
+
+import {
+  Box,
+  BoxTitle,
+  Category,
+  CategoryIcon,
+  CategoryWrap,
+  Container,
+  Icons,
+  Input,
+  Text,
+  Title,
+  Wrapper
+} from './style';
+
 export default function Home () {
+
+  const category = [
+    { id: 1, name: 'chemistry#', url: icon1, mg: true },
+    { id: 2, name: 'maths#', url: icon2, mg: true },
+    { id: 3, name: 'physics#', url: icon3, mg: true },
+    { id: 4, name: 'astronomy#', url: icon4, mg: false },
+  ]
+
   return (
-    <div>Home</div>
+    <Wrapper>
+      <Container>
+        <BoxTitle>
+          <Title type='lg'>Tutor Chat</Title>
+          <BoxTitle type='min'>
+            <Icons.Message />
+            <Icons.Telegram />
+          </BoxTitle>
+        </BoxTitle>
+        <Title type='md'>Start Learning</Title>
+        <Box>
+          <Icons.Search />
+          <Input type='text' placeholder='what you want to learn?' />
+        </Box>
+        <CategoryWrap>
+          {
+            category.map(({ id, name, url, mg }) => {
+              return (
+                <Category key={id} mg={mg}>
+                  <CategoryIcon src={url} />
+                  <Text>{name}</Text>
+                </Category>
+              )
+            })
+          }
+        </CategoryWrap>
+      </Container>
+    </Wrapper>
   )
 }
---
+++ src/components/Home/style.js
@@ -0,0 +1,125 @@
+import { styled } from "styled-components";
+
+import { ReactComponent as message } from "../../assets/icons/home_message.svg";
+import { ReactComponent as telegram } from "../../assets/icons/tg_message.svg";
+import { ReactComponent as search } from "../../assets/icons/home_search.svg";
+
+const Icons = styled.div``;
+Icons.Message = styled(message)``;
+Icons.Telegram = styled(telegram)``;
+Icons.Search = styled(search)`
+position: absolute;
+left: 20px;
+top: 50%;
+transform: translateY(-40%);
+`;
+
+const Wrapper = styled.div`
+width: 100%;
+margin-top: 50px;
+`
+
+const Container = styled.div`
+padding: 0 7px;
+`
+
+const getType = ({ type }) => {
+  switch (type) {
+    case 'min': return '80px';
+    default: return '100%';
+  }
+}
+
+const BoxTitle = styled.div`
+display: flex;
+width:${getType};
+justify-content: space-between;
+align-items: center;
+`
+
+const getSize = ({ type }) => {
+  switch (type) {
+    case 'lg': return {
+      color: '#000',
+      fontSize: '25px',
+      fontWeight: '600',
+    };
+    case 'md': return {
+      color: '#000',
+      fontSize: '23px',
+      fontWeight: '400',
+      marginTop: '40px',
+    };
+    default: return {
+      color: '#000',
+      fontSize: '16px',
+      fontWeight: '400',
+    };
+  }
+}
+
+const Title = styled.div`
+${getSize}
+`
+
+const Box = styled.div`
+position: relative;
+margin-top: 20px;
+`;
+const Input = styled.input`
+width: 100%;
+height: 44px;
+font-size: 14px;
+font-weight: 400;
+padding-left: 50px;
+border: none;
+outline: none;
+::placeholder{
+  color: #9A9A9A;
+}
+`
+
+const CategoryWrap = styled.div`
+display: flex;
+justify-content: center;
+flex-wrap: wrap;
+align-items: center;
+`
+
+const Category = styled.div`
+width: fit-content;
+padding: 8px 10px;
+background: #F4F4F4;
+border-radius: 11px;
+display: flex;
+justify-content: space-between;
+align-items: center;
+margin-right: ${({ mg }) => mg ? '10px' : '0'};
+margin-top: 20px;
+`
+
+const Text = styled.span`
+font-size: 14px;
+font-weight: 400;
+color: #5F5F5F;
+margin-left: 5px;
+`
+
+const CategoryIcon = styled.img``;
+
+
+
+
+export {
+  Wrapper,
+  BoxTitle,
+  Title,
+  Icons,
+  Container,
+  Box,
+  Input,
+  CategoryWrap,
+  Category,
+  Text,
+  CategoryIcon,
+}
